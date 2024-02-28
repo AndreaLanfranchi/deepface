16eebbf72998924a0205e68fb30643f8abbd7eba
Andrea Lanfranchi <andrea.lanfranchi@gmail.com>
2024-02-28 12:00:37 +0100

    ArcFace.py - wrap all methods in the class

--- tests/stream.py
+++ tests/stream.py
@@ -1,6 +1,6 @@
 from deepface import DeepFace
 
-DeepFace.stream("dataset") #opencv
+DeepFace.stream("dataset", model_name="ArcFace") #opencv
 #DeepFace.stream("dataset", detector_backend = 'opencv')
 #DeepFace.stream("dataset", detector_backend = 'ssd')
 #DeepFace.stream("dataset", detector_backend = 'mtcnn')
