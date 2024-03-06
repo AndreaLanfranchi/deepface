64dad7edea5d69eab6af7f4b1e6e11a9f7548258
Andrea Lanfranchi <andrea.lanfranchi@gmail.com>
2024-03-06 18:45:29 +0100

    Package_utils does not require logging

--- deepface/commons/package_utils.py
+++ deepface/commons/package_utils.py
@@ -1,12 +1,6 @@
 # 3rd party dependencies
 import tensorflow
 
-# package dependencies
-from deepface.commons.logger import Logger
-
-logger = Logger(module="commons.package_utils")
-
-
 def get_tf_major_version() -> int:
     """
     Find tensorflow's major version
