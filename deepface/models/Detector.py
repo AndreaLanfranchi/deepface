e333e409d90801ee2353410d248cb8a8c34ea95b
Andrea Lanfranchi <andrea.lanfranchi@gmail.com>
2024-03-07 09:00:05 +0100

    Introduce detection results

--- deepface/models/Detector.py
+++ deepface/models/Detector.py
@@ -110,6 +110,83 @@ class Detector(ABC):
 
         return instance
 
+class Point:
+    """
+    This class is used to represent a point in a 2D space
+    """
+    def __init__(self, x: int, y: int):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return f"Point(x={self.x}, y={self.y})"
+
+    def __repr__(self):
+        return self.__str__()
+
+class RangeInt:
+    """
+    This class is used to represent a range of integers as [start, end]
+    """
+    def __init__(self, start: int, end: int):
+        self.start = max(start, 0)
+        self.end = max(end, 0)
+        self.end = max(self.end, self.start)
+
+    def __str__(self):
+        return f"RangeInt(start={self.start}, end={self.end})"
+
+    def __repr__(self):
+        return self.__str__()
+
+class DetectionResult:
+    """
+    This class is used to represent the result of a face detection.
+    It contains the detected facial area and the confidence of the detection.
+    """
+
+    def __init__(
+        self,
+        detector: Detector,
+        source: numpy.ndarray,
+        y: RangeInt,
+        x: RangeInt,
+        left_eye: Optional[Point] = None,
+        right_eye: Optional[Point] = None,
+        confidence: Optional[float] = None,
+    ):
+        self._detector = detector
+        self._source = source
+        self._y = y
+        self._x = x
+        self._left_eye = left_eye
+        self._right_eye = right_eye
+        self._confidence = confidence
+
+    @property
+    def height(self) -> int:
+        return self._y.end - self._y.start
+
+    @property
+    def width(self) -> int:
+        return self._x.end - self._x.start
+
+    @property
+    def area(self) -> int:
+        return self.height * self.width
+
+    @property
+    def empty(self) -> bool:
+        return self.area == 0
+
+    @property
+    def face(self) -> numpy.ndarray:
+        return self._source[self._y.start : self._y.end, self._x.start : self._x.end]
+
+    @property
+    def confidence(self) -> Optional[float]:
+        return self._confidence
+
 class FacialAreaRegion:
     x: int
     y: int
