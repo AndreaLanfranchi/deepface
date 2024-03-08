5c84cd218857a3e12cc5d28bc135dc2d2e4a0378
Andrea Lanfranchi <andrea.lanfranchi@gmail.com>
2024-03-08 10:49:08 +0100

    Move InPictureFace

--- deepface/core/detector.py
+++ deepface/core/detector.py
@@ -101,56 +101,6 @@ class Detector(ABC):
 
         return instance
 
-
-class DetectionResult:
-    """
-    This class is used to represent the result of a face detection.
-    It contains the detected facial area and the confidence of the detection.
-    """
-
-    def __init__(
-        self,
-        detector: Detector,
-        source: numpy.ndarray,
-        y: RangeInt,
-        x: RangeInt,
-        left_eye: Optional[Point] = None,
-        right_eye: Optional[Point] = None,
-        confidence: Optional[float] = None,
-    ):
-        self._detector = detector
-        self._source = source
-        self._y = y
-        self._x = x
-        self._left_eye = left_eye
-        self._right_eye = right_eye
-        self._confidence = confidence
-
-    @property
-    def height(self) -> int:
-        return self._y.end - self._y.start
-
-    @property
-    def width(self) -> int:
-        return self._x.end - self._x.start
-
-    @property
-    def area(self) -> int:
-        return self.height * self.width
-
-    @property
-    def empty(self) -> bool:
-        return self.area == 0
-
-    @property
-    def face(self) -> numpy.ndarray:
-        return self._source[self._y.start : self._y.end, self._x.start : self._x.end]
-
-    @property
-    def confidence(self) -> Optional[float]:
-        return self._confidence
-
-
 class FacialAreaRegion:
     x: int
     y: int
