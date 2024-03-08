d6f6e6c1bb96c503ea3168907544d8cb344ee1d4
Andrea Lanfranchi <andrea.lanfranchi@gmail.com>
2024-03-08 17:24:35 +0100

    Singleton instance method for Demography

--- deepface/models/Demography.py
+++ deepface/models/Demography.py
@@ -1,7 +1,13 @@
 from typing import Optional, Union
 from abc import ABC, abstractmethod
+import time
 import numpy
-from deepface.commons import package_utils
+
+from deepface import extendedmodels
+from deepface.core import reflection
+from deepface.commons.logger import Logger
+
+logger = Logger()
 
 
 # Notice that all facial attribute analysis models must be inherited from this class
@@ -16,3 +22,59 @@ class Demography(ABC):
     @property
     def name(self) -> str:
         return "<undefined>" if self._name is None else self._name
+
+    @staticmethod
+    def instance(name: str, singleton: bool = True) -> "Demography":
+        """
+        `Demography` factory method.
+
+        Args:
+            `name (str)`: The name of the analyzer to instantiate
+            `singleton (bool)`: If True, the same instance will be returned
+
+        Return:
+            An instance of the `Demography` subclass matching the given name
+
+        Raises:
+            `ValueError`: If the detector name empty
+            `NotImplementedError`: If the detector name is unknown
+
+        """
+        name = name.lower().strip()
+        if len(name) == 0:
+            raise ValueError("Empty analyzer name")
+
+        global analyzers_instances  # singleton design pattern
+        if not "analyzers_instances" in globals():
+            analyzers_instances = {}
+
+        global available_analyzers
+        if not "available_analyzers" in globals():
+            available_analyzers = reflection.get_derived_classes(
+                package=extendedmodels, base_class=Demography
+            )
+
+        if name not in available_analyzers.keys():
+            raise NotImplementedError(f"Unknown analyzer [{name}]")
+
+        tic = time.time()
+        try:
+            if not singleton:
+                instance = available_analyzers[name]()
+                logger.debug(
+                    f"Instantiated analyzer [{name}] ({time.time() - tic:.3f} seconds)"
+                )
+            else:
+                if name not in analyzers_instances.keys():
+                    analyzers_instances[name] = available_analyzers[name]()
+                instance = analyzers_instances[name]
+                logger.debug(
+                    f"Instantiated analyzer [{name}] ({time.time() - tic:.3f} seconds)"
+                )
+        except Exception as ex:
+            logger.critical(
+                f"{type(ex).__name__} on analyzer [{name}] Error: {ex.args}"
+            )
+            raise ex
+
+        return instance
