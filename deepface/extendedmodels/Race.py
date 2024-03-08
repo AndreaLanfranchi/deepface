505f5015faa5ad88dac149208fb32310ec7b4a87
Andrea Lanfranchi <andrea.lanfranchi@gmail.com>
2024-03-08 16:54:33 +0100

    Refactor Race.py

--- deepface/extendedmodels/Race.py
+++ deepface/extendedmodels/Race.py
@@ -29,44 +29,31 @@ class RaceClient(Demography):
     """
     Race model class
     """
+    _model: Model  # The actual model used for the analysis
 
     def __init__(self):
-        self.model = load_model()
-        self.model_name = "Race"
+        self._name = str(__name__.rsplit(".", maxsplit=1)[-1])
+        self.__initialize()
 
     def predict(self, img: numpy.ndarray) -> numpy.ndarray:
-        return self.model.predict(img, verbose=0)[0, :]
+        return self._model.predict(img, verbose=0)[0, :]
 
+    def __initialize(self):
 
-def load_model() -> Model:
-    """
-    Construct race model, download its weights and load
-    """
+        classes = 6  # TDOO: What is this magic number?
+        with VGGFace.base_model() as base_model:
+            base_model_output = Sequential()
+            base_model_output = Convolution2D(classes, (1, 1), name="predictions")(base_model.layers[-4].output)
+            base_model_output = Flatten()(base_model_output)
+            base_model_output = Activation("softmax")(base_model_output)
+            self._model = Model(inputs=base_model.input, outputs=base_model_output)
 
-    model = VGGFace.base_model()
+        file_name = "race_model_single_batch.h5"
+        url = f"https://github.com/serengil/deepface_models/releases/download/v1.0/{file_name}"
+        output = os.path.join(folder_utils.get_weights_dir(), file_name)
 
-    # --------------------------
+        if os.path.isfile(output) != True:
+            logger.info(f"Download : {file_name}")
+            gdown.download(url, output, quiet=False)
 
-    classes = 6
-    base_model_output = Sequential()
-    base_model_output = Convolution2D(classes, (1, 1), name="predictions")(model.layers[-4].output)
-    base_model_output = Flatten()(base_model_output)
-    base_model_output = Activation("softmax")(base_model_output)
-
-    # --------------------------
-
-    race_model = Model(inputs=model.input, outputs=base_model_output)
-
-    # --------------------------
-
-    # load weights
-    file_name = "race_model_single_batch.h5"
-    url = f"https://github.com/serengil/deepface_models/releases/download/v1.0/{file_name}"
-    output = os.path.join(folder_utils.get_weights_dir(), file_name)
-
-    if os.path.isfile(output) != True:
-        logger.info(f"Download : {file_name}")
-        gdown.download(url, output, quiet=False)
-
-    race_model.load_weights(output)
-    return race_model
+        self._model.load_weights(output)
