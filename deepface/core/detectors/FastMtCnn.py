eb6b0c0ef71f6a0e515ae5e76782438f71317726
Andrea Lanfranchi <andrea.lanfranchi@gmail.com>
2024-04-28 12:36:49 +0200

    Amend FastMtcnn detection
    Case when there're no faces or key_points == false

--- deepface/core/detectors/FastMtCnn.py
+++ deepface/core/detectors/FastMtCnn.py
@@ -73,86 +73,101 @@ class Detector(DetectorBase):
         # before converting it to RGB
         img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-        boxes, probs, keypoints_list = self._detector.detect(img_rgb, landmarks=True)
+        detection_results = self._detector.detect(img_rgb, landmarks=key_points)
 
-        for box, prob, keypoints in zip(boxes, probs, keypoints_list):
+        if detection_results is not None and not any(
+            item is None for item in detection_results
+        ):
+            if len(detection_results) == 2:
+                boxes, probs = detection_results
+                keypoints_list = numpy.full((len(boxes), 1, 2), numpy.nan)
+            elif len(detection_results) == 3:
+                boxes, probs, keypoints_list = detection_results
 
-            confidence = round(float(prob), 5)
-            if confidence < min_confidence:
-                continue  # Confidence too low
+            for box, prob, keypoints in zip(boxes, probs, keypoints_list):
 
-            if box is None or not isinstance(box, numpy.ndarray) or box.shape[0] != 4:
-                continue  # No detection or tampered data
+                confidence = round(float(prob), 5)
+                if confidence < min_confidence:
+                    continue  # Confidence too low
 
-            x1, y1, x2, y2 = (int(round(val)) for val in box[:4])
-            x_range = RangeInt(start=x1, end=min(x2, img_width))
-            y_range = RangeInt(start=y1, end=min(y2, img_height))
-            if x_range.span <= min_dims.width or y_range.span <= min_dims.height:
-                continue  # Invalid or empty detection
+                if (
+                    box is None
+                    or not isinstance(box, numpy.ndarray)
+                    or box.shape[0] != 4
+                ):
+                    continue  # No detection or tampered data
 
-            bounding_box: BoundingBox = BoundingBox(
-                top_left=Point(x=x_range.start, y=y_range.start),
-                bottom_right=Point(x=x_range.end, y=y_range.end),
-            )
+                x1, y1, x2, y2 = (int(round(val)) for val in box[:4])
+                x_range = RangeInt(start=x1, end=min(x2, img_width))
+                y_range = RangeInt(start=y1, end=min(y2, img_height))
+                if x_range.span <= min_dims.width or y_range.span <= min_dims.height:
+                    continue  # Invalid or empty detection
 
-            points: Optional[Dict[str, Optional[Point]]] = None
-            if (
-                key_points
-                and isinstance(keypoints, numpy.ndarray)
-                and keypoints.shape[0] > 0
-                and keypoints.shape[1] == 2  # 2D coordinates
-            ):
-                # 0: left eye
-                # 1: right eye
-                # 2: nose,
-                # 3: left mouth
-                # 4: right mouth
-
-                points = dict[str, Optional[Point]]()
-                if keypoints.shape[0] >= 2:
-                    le_point = Point(
-                        x=int(round(keypoints[1][0])),
-                        y=int(round(keypoints[1][1])),
-                    )
-                    re_point = Point(
-                        x=int(round(keypoints[0][0])),
-                        y=int(round(keypoints[0][1])),
-                    )
-                    points.update({"lec": le_point, "rec": re_point})
-
-                if keypoints.shape[0] >= 3:
-                    n_point = Point(
-                        x=int(round(keypoints[2][0])),
-                        y=int(round(keypoints[2][1])),
-                    )
-                    points.update({"nt": n_point})
-
-                if keypoints.shape[0] >= 5:
-                    lm_point = Point(
-                        x=int(round(keypoints[3][0])),
-                        y=int(round(keypoints[3][1])),
-                    )
-                    rm_point = Point(
-                        x=int(round(keypoints[4][0])),
-                        y=int(round(keypoints[4][1])),
-                    )
-                    cm_point = Point(
-                        x=(lm_point.x + rm_point.x) // 2,
-                        y=(lm_point.y + rm_point.y) // 2,
-                    )
-                    points.update({"mlc": lm_point, "mrc": rm_point, "mc": cm_point})
-
-            try:
-                # This might raise an exception if the values are out of bounds
-                detected_faces.append(
-                    DetectedFace(
-                        confidence=confidence,
-                        bounding_box=bounding_box,
-                        key_points=points,
-                    )
+                bounding_box: BoundingBox = BoundingBox(
+                    top_left=Point(x=x_range.start, y=y_range.start),
+                    bottom_right=Point(x=x_range.end, y=y_range.end),
                 )
-            except Exception as e:
-                logger.debug(f"Error: {e}")
+
+                points: Optional[Dict[str, Point]] = None
+                if (
+                    key_points
+                    and isinstance(keypoints, numpy.ndarray)
+                    and keypoints.shape[0] > 0
+                    and keypoints.shape[1] == 2  # 2D coordinates
+                ):
+                    # 0: left eye
+                    # 1: right eye
+                    # 2: nose,
+                    # 3: left mouth
+                    # 4: right mouth
+
+                    points = dict[str, Point]()
+                    if keypoints.shape[0] >= 2:
+                        le_point = Point(
+                            x=int(round(keypoints[1][0])),
+                            y=int(round(keypoints[1][1])),
+                        )
+                        re_point = Point(
+                            x=int(round(keypoints[0][0])),
+                            y=int(round(keypoints[0][1])),
+                        )
+                        points.update({"lec": le_point, "rec": re_point})
+
+                    if keypoints.shape[0] >= 3:
+                        n_point = Point(
+                            x=int(round(keypoints[2][0])),
+                            y=int(round(keypoints[2][1])),
+                        )
+                        points.update({"nt": n_point})
+
+                    if keypoints.shape[0] >= 5:
+                        lm_point = Point(
+                            x=int(round(keypoints[3][0])),
+                            y=int(round(keypoints[3][1])),
+                        )
+                        rm_point = Point(
+                            x=int(round(keypoints[4][0])),
+                            y=int(round(keypoints[4][1])),
+                        )
+                        cm_point = Point(
+                            x=(lm_point.x + rm_point.x) // 2,
+                            y=(lm_point.y + rm_point.y) // 2,
+                        )
+                        points.update(
+                            {"mlc": lm_point, "mrc": rm_point, "mc": cm_point}
+                        )
+
+                try:
+                    # This might raise an exception if the values are out of bounds
+                    detected_faces.append(
+                        DetectedFace(
+                            confidence=confidence,
+                            bounding_box=bounding_box,
+                            key_points=points,
+                        )
+                    )
+                except Exception as e:
+                    logger.debug(f"Error: {e}")
 
         if 0 == len(detected_faces) and raise_notfound:
             raise FaceNotFoundError("No face detected. Check the input image.")
